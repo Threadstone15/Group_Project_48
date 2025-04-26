33dd0f20f7b209a3f6b7b6deff239382d7ad5c62
Anupa Siriwardana <anupasiriwardana123@gmail.com>
2025-04-27 03:03:01 +0530

    added a minor change to trainer calendar

--- frontend/js/trainer/trainerHome.js
+++ frontend/js/trainer/trainerHome.js
@@ -207,4 +207,18 @@ export function initTrainer_home() {
     // Init
     fetchAndDisplayNotices();
     updateGymData();
+
+    window.addEventListener('message', (event) => {
+        if (event.data.call === 'SHOW_TOAST') {
+          const container = document.getElementById('global-toast-container');
+          const toast = document.createElement('div');
+          toast.className = `global-toast ${event.data.toastType}`;
+          toast.innerHTML = event.data.message;
+          container.appendChild(toast);
+    
+          setTimeout(() => {
+            toast.remove();
+          }, 4000);
+        }
+      });
 }
